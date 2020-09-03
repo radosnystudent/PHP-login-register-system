aa4da5709c3724a3a072c04864e1f2115d24e699
Jeremiasz Pauszek <35640987+radosnystudent@users.noreply.github.com>
2020-09-03 11:13:48 +0200

    confing file for mail sending

---
+++ class/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+class Config {
+
+    const SMTP_HOST = '';
+    const SMTP_PORT = 2525;
+    const SMTP_USER = '';
+    const SMTP_PASSWORD = '';
+    const DEV_URL = 'http://localhost:8888/login';
+    const PROD_URL = '';
+
+}
+
+
+?>
